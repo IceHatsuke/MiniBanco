b0fb4baad1563e33e0585c2a8d4a5098a3da38c4
IceHatsuke <gabriele16022005@gmail.com>
2026-03-16 19:47:34 -0300

    Feat: Criar estrutura inicial da classe minibanco

---
+++ MiniBanco.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class MiniBanco {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        double saldo  = 0.0;
+        int    opcao  = -1;
+
+        System.out.println("Digite o seu nome: ");
+        String nome = scanner.next();
+        System.out.printf("Olá, %s! Saldo inicial: R$ %.2f\n, nome, saldo");
+
+        scanner.close();
+    }
+}
+
