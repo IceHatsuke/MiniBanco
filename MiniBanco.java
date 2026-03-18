4c3c5ca7eb7789e2f42acb3af6443a2b49192f82
IceHatsuke <gabriele16022005@gmail.com>
2026-03-18 20:10:28 -0300

    Feat: adiciona extrato da sessão com array e strings

--- MiniBanco.java
+++ MiniBanco.java
@@ -45,9 +45,30 @@ public class MiniBanco {
         return valor > 0;
     }
 
+    static void exibirExtrato(String[] extrato, int totalLinhas){
+        System.out.println("\n--Extrato---------------");
+        if(totalLinhas == 0){
+            System.out.println("Nenhuma movimentação");
+        }
+        else{
+            for(int i = 0; i < totalLinhas; i++){
+                System.out.println("" + extrato[i]);
+            }
+        }
+        System.out.println("-----------------------");
+    }
+
+    static int registrar(String[] extrato, int totalLinhas, String linha){
+        extrato[totalLinhas] = linha;
+        return totalLinhas + 1;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        String[] extrato = new String[50];
+        int      totalLinhas = 0;
+
         double saldo  = 0.0;
         int    opcao  = -1;
 
@@ -71,6 +92,7 @@ public class MiniBanco {
                     saldo = depositar(saldo,valor);
                     System.out.println("Deposito Realizado");
                     exibirSaldo(saldo);
+                    totalLinhas = registrar(extrato, totalLinhas, String.format("DEPÓSITO +R$ %.2f => Saldo R$ %.2f", valor, saldo));
                 }
 
                 //System.out.println(" [Depositar - em breve]");
@@ -91,15 +113,18 @@ public class MiniBanco {
                     double taxa = valorSaque * TAXA_DE_SAQUE;
                     saldo = sacar(saldo, valorSaque);
                     System.out.printf("Saque realizado. Taxa cobrada: R$ %.2f%n", taxa);
+                    totalLinhas = registrar(extrato, totalLinhas, String.format("SAQUE -R$ %.2f => Saldo: R$ %.2f", valorSaque, saldo));
                 }
             }
             else if(opcao == 3){
-                System.out.println(" [Consultar saldo]");
+                exibirSaldo(saldo);
             }
             else if(opcao == 4){
-                System.out.println(" [Extrato - em breve]");
+                exibirExtrato(extrato, totalLinhas);
+                //System.out.println(" [Extrato - em breve]");
             }
             else if(opcao == 0){
+                exibirExtrato(extrato, totalLinhas);
                 System.out.println("Até logo, " + nome + "!");
             }
             else{
